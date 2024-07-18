64de2bd40022bcdb50815a13b5e113df2d3f7e3f
souta1125 <97822981+souta1125@users.noreply.github.com>
2024-07-19 02:58:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,35 @@ function list(e,html){
   let t = html
   ,   img = e[0]
   ,   name = e[1]
+  ,   sponsor = e[2]
+  ,   description = e[3]
+  ,   operation = e[4]
   ,   waitingTime = e[5]
+  ,   operationTime = e[6]
   ,   tags = e[7];
 
   t.find('img').attr('src', img);
   t.find('.name').text(name);
+  t.find('.sponsor').text(sponsor);
+  t.find('.description').text(description);
+  t.find('.operation').text(operation);
+  for (let j = 0; j < operationTime.length; j++){
+    if(operationTime[j]){
+      t.find('.operationTime').append('<span>' + operationTime[j] + '</span>');
+    };
+  };
   if(tags.length != 0){
-    for (let j = 0; j < tags.length; j++) {
+    for (let j = 0; j < tags.length; j++){
       if(tags[j]){
         t.find('.tags').append('<span>' + tags[j] + '</span>');
-      }
-    }
+      };
+    };
   }
   if(waitingTime){
-    t.find('.waitingTime').html('<span>'+waitingTime+'分待ち</span>');
+    t.find('.waitingTime').html('<span>'+waitingTime+'</span>分待ち');
+  } else{
+    t.find('.waitingTime').html('<span>'+operation+'</span>');
   }
-
   return html
 }
 
@@ -35,13 +48,54 @@ fetch(apiURL)
   return fetch_data.json();
 })
 .then(function(json){
-  for(let i in json){
-    let skeleton = $('#js-list_skeleton')
-    ,   html = skeleton.children().clone();
+  for (let i = 0; i < json.length; i++) {
+    let html = $('#js-list_skeleton').find('.list_item').clone(true);
 
     list(json[i], html);
 
-    $('#js-list').append(html);
+    $('#js-list').append(html)
   }
-  loading()
+  loading();
+})
+
+// app-list_detail
+$('.list_item').click(function(){
+  let t = $(this)
+  ,   name = t.find('.name').text()
+  ,   img = t.find('img').attr('src')
+  ,   sponsor = t.find('.sponsor').text()
+  ,   waitingTime = t.find('.waitingTime > span').text()
+  ,   description = t.find('.description').text()
+  ,   operation = t.find('.operation').text()
+  ,   operationTime = t.find('.operationTime').html()
+  ,   u = $('#js-list_detail')
+  ,   html = $('#js-list_detail_skeleton').children().clone();
+
+  html.find('img').attr('src', img);
+  html.find('.name').text(name);
+  if(sponsor){
+    html.find('.sponsor').text(sponsor);
+  }
+  if(waitingTime){
+    html.find('.waitingTime').html('<span>'+ waitingTime +'</span>分待ち');
+  }
+  html.find('.description').text(description);
+  html.find('.operation').text(operation);
+
+  u.fadeIn(200);
+
+  u.find('#js-detail_wrapper').empty()
+  u.find('#js-detail_wrapper').append(html)
+})
+
+// closeBtn
+$('.closeBtn').click(function(){
+  let status = $(this).attr('aria-disabled');
+
+  if(status == "false"){
+    let t = $(this)
+    ,   u = t.parents('.wrapper');
+
+    u.fadeOut(200)
+  }
 })
